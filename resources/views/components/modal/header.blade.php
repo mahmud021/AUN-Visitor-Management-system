28d354580f69501c68b2fb16560d8d4c473addf4
mahmud <mahmudmodibbo21@gmail.com>
2025-03-06 23:04:17 +0100

    Adde submit on header

--- resources/views/components/modal/header.blade.php
+++ resources/views/components/modal/header.blade.php
@@ -1,6 +1,8 @@
 <div class="flex justify-between items-center py-3 px-4 border-b dark:border-neutral-700">
     <h3 id="hs-static-backdrop-modal-label" class="font-bold text-gray-800 dark:text-white">
-        Create User
+        <x-primary-button type="submit">
+            Submit
+        </x-primary-button>
     </h3>
     <button type="button"
             class="size-8 inline-flex justify-center items-center gap-x-2 rounded-full border border-transparent bg-gray-100 text-gray-800 hover:bg-gray-200 focus:outline-none focus:bg-gray-200 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-700 dark:hover:bg-neutral-600 dark:text-neutral-400 dark:focus:bg-neutral-600"
