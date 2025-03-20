103cf6d4382ac11ec4874a6a6343e0041491766b
mahmud <mahmudmodibbo21@gmail.com>
2025-03-20 03:55:04 +0100

    form components

--- resources/views/components/form/select.blade.php
+++ resources/views/components/form/select.blade.php
@@ -1,12 +1,14 @@
 @props(['name', 'label'])
 
 <div>
-    <label for="{{ $name }}" class="block mb-2 text-sm text-gray-700 font-medium dark:text-white">
+    <label for="{{ $name }}" class="block mb-2 text-sm text-white font-medium">
         {{ $label }}
     </label>
     <select name="{{ $name }}"
             id="{{ $name }}"
-        {{ $attributes->merge(['class' => 'py-3 px-4 pe-9 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-brand-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600']) }}>
+        {{ $attributes->merge([
+            'class' => 'py-3 px-4 pe-9 block w-full border-neutral-700 rounded-lg text-sm bg-brand-900 text-neutral-400 placeholder-neutral-500 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none focus:ring-neutral-600'
+        ]) }}>
         {{ $slot }}
     </select>
 </div>
--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -3,5 +3,7 @@
 <input
     @disabled($disabled)
     value="{{ $value }}"
-    {{ $attributes->merge(['class' => 'border-gray-300 dark:border-neutral-700 bg-brand-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm']) }}
+    {{ $attributes->merge([
+        'class' => 'border-neutral-700 bg-brand-900 text-gray-300 focus:border-indigo-600 focus:ring-indigo-600 rounded-md shadow-sm'
+    ]) }}
 >
