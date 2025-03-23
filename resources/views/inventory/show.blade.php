ffaf77f2d1d0b94f5977909d4777c51c7a992f88
mahmud <mahmudmodibbo21@gmail.com>
2025-03-23 04:58:40 +0100

    image resize problems

--- resources/views/inventory/show.blade.php
+++ resources/views/inventory/show.blade.php
@@ -11,32 +11,33 @@
             <div class="grid sm:grid-cols-2 sm:items-center gap-8">
 
                 <!-- Carousel (right side) -->
+                <!-- Carousel (right side) -->
                 <div class="sm:order-2 relative">
                     <div data-hs-carousel='{
-          "isAutoHeight": true,
-          "loadingClasses": "opacity-0",
-          "dotsItemClasses": "hs-carousel-active:bg-blue-700 hs-carousel-active:border-blue-700 size-3 border border-gray-400 rounded-full cursor-pointer dark:border-neutral-600 dark:hs-carousel-active:bg-blue-500 dark:hs-carousel-active:border-blue-500"
-        }' class="relative">
+      "isAutoHeight": true,
+      "loadingClasses": "opacity-0",
+      "dotsItemClasses": "hs-carousel-active:bg-blue-700 hs-carousel-active:border-blue-700 size-3 border border-gray-400 rounded-full cursor-pointer dark:border-neutral-600 dark:hs-carousel-active:bg-blue-500 dark:hs-carousel-active:border-blue-500"
+    }' class="relative">
                         <div class="hs-carousel relative overflow-hidden w-full bg-transparent rounded-lg">
                             <div class="hs-carousel-body flex flex-nowrap overflow-hidden transition-[height,transform] duration-700 opacity-0">
 
                                 <!-- First Slide -->
                                 <div class="hs-carousel-slide h-80 lg:h-96">
-                                    <div class="bg-no-repeat bg-center bg-contain h-full w-full"
+                                    <div class="h-full w-full bg-gray-100 dark:bg-neutral-800 bg-no-repeat bg-center bg-contain"
                                          style="background-image: url('{{ asset('storage/appliance_images/1.jpeg') }}')">
                                     </div>
                                 </div>
 
                                 <!-- Second Slide -->
                                 <div class="hs-carousel-slide h-80 lg:h-96">
-                                    <div class="bg-no-repeat bg-center bg-contain h-full w-full"
+                                    <div class="h-full w-full bg-gray-200 dark:bg-neutral-700 bg-no-repeat bg-center bg-contain"
                                          style="background-image: url('{{ asset('storage/appliance_images/2.jpeg') }}')">
                                     </div>
                                 </div>
 
                                 <!-- Third Slide -->
                                 <div class="hs-carousel-slide h-80 lg:h-96">
-                                    <div class="bg-no-repeat bg-center bg-contain h-full w-full"
+                                    <div class="h-full w-full bg-gray-300 dark:bg-neutral-600 bg-no-repeat bg-center bg-contain"
                                          style="background-image: url('{{ asset('storage/appliance_images/3.jpeg') }}')">
                                     </div>
                                 </div>
@@ -45,26 +46,31 @@
                         </div>
 
                         <button type="button" class="hs-carousel-prev hs-carousel-disabled:opacity-50 hs-carousel-disabled:pointer-events-none absolute inset-y-0 start-0 inline-flex justify-center items-center w-11.5 h-full text-gray-800 hover:bg-gray-800/10 focus:outline-hidden focus:bg-gray-800/10 rounded-s-lg dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10">
-          <span class="text-2xl" aria-hidden="true">
-            <svg class="shrink-0 size-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-              <path d="m15 18-6-6 6-6"></path>
-            </svg>
-          </span>
+      <span class="text-2xl" aria-hidden="true">
+        <svg class="shrink-0 size-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+             viewBox="0 0 24 24" fill="none" stroke="currentColor"
+             stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+          <path d="m15 18-6-6 6-6"></path>
+        </svg>
+      </span>
                             <span class="sr-only">Previous</span>
                         </button>
 
                         <button type="button" class="hs-carousel-next hs-carousel-disabled:opacity-50 hs-carousel-disabled:pointer-events-none absolute inset-y-0 end-0 inline-flex justify-center items-center w-11.5 h-full text-gray-800 hover:bg-gray-800/10 focus:outline-hidden focus:bg-gray-800/10 rounded-e-lg dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10">
                             <span class="sr-only">Next</span>
                             <span class="text-2xl" aria-hidden="true">
-            <svg class="shrink-0 size-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-              <path d="m9 18 6-6-6-6"></path>
-            </svg>
-          </span>
+        <svg class="shrink-0 size-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+             viewBox="0 0 24 24" fill="none" stroke="currentColor"
+             stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+          <path d="m9 18 6-6-6-6"></path>
+        </svg>
+      </span>
                         </button>
 
                         <div class="hs-carousel-pagination flex justify-center absolute bottom-3 start-0 end-0 gap-x-2"></div>
                     </div>
                 </div>
+
 
                 <!-- Details (left side) -->
                 <div class="sm:order-1">
