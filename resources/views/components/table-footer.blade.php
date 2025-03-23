051a5bc824d563c96ddd0c25caa19e5eef83ccd7
mahmud <mahmudmodibbo21@gmail.com>
2025-03-23 05:37:26 +0100

    pagination optional to prevent errors

--- resources/views/components/table-footer.blade.php
+++ resources/views/components/table-footer.blade.php
@@ -1,8 +1,10 @@
 <div class="px-6 py-4 grid gap-3 md:flex md:justify-between md:items-center border-t border-gray-600">
     <div></div>
-    <div>
-        <div class="inline-flex gap-x-2">
-            {{ $pagination }}
+    @isset($pagination)
+        <div>
+            <div class="inline-flex gap-x-2">
+                {{ $pagination }}
+            </div>
         </div>
-    </div>
+    @endisset
 </div>
