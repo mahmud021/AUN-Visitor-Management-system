dafefa0f3f94782a792b1ee5e78101948ace5aed
mahmud <mahmudmodibbo21@gmail.com>
2025-03-25 23:31:23 +0100

    app settings craeted

---
+++ database/migrations/2025_03_25_233041_create_app_settings_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('app_settings', function (Blueprint $table) {
+            $table->id();
+            $table->time('visitor_start_time')->nullable();
+            $table->time('visitor_end_time')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('app_settings');
+    }
+};
