d2d87408ebe69840543b59641228c52b636947df
mahmud <mahmudmodibbo21@gmail.com>
2025-03-26 18:13:04 +0100

    visitor show working and neat

--- resources/views/users/visitor-logs.blade.php
+++ resources/views/users/visitor-logs.blade.php
@@ -1,12 +1,55 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Visitor Logs') }}
+            {{ __('User Information and Logs') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div>
+                <div class="px-4 sm:px-0">
+                    <h3 class="text-base/7 font-semibold text-brand-50"></h3>
+                    <p class="mt-1 max-w-2xl text-sm/6 text-brand-300">Personal details and application.</p>
+                </div>
+                <div class="mt-6 border-t border-brand-800 ">
+                    <dl class="divide-y divide-brand-800">
+                        <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                            <dt class="text-sm/6 font-medium text-brand-50">Full name</dt>
+                            <dd class="mt-1 text-sm/6 text-brand-200 sm:col-span-2 sm:mt-0">{{$user->first_name}} {{$user->last_name}}</dd>
+                        </div>
+                        <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                            <dt class="text-sm/6 font-medium text-brand-50">Email address</dt>
+                            <dd class="mt-1 text-sm/6 text-brand-200 sm:col-span-2 sm:mt-0">{{$user->email}}</dd>
+                        </div>
+                        <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                            <dt class="text-sm/6 font-medium text-brand-50">School ID</dt>
+                            <dd class="mt-1 text-sm/6 text-brand-200 sm:col-span-2 sm:mt-0">{{$user->user_details->school_id}}</dd>
+                        </div>
+
+                        <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                            <dt class="text-sm/6 font-medium text-brand-50">Role</dt>
+                            <dd class="mt-1 text-sm/6 text-brand-200 sm:col-span-2 sm:mt-0">{{$user->user_details->role}}</dd>
+                        </div>
+                        <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                            <dt class="text-sm/6 font-medium text-brand-50">Telephone</dt>
+                            <dd class="mt-1 text-sm/6 text-brand-200 sm:col-span-2 sm:mt-0">{{$user->user_details->telephone}}</dd>
+                        </div>
+                        <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                            <dt class="text-sm/6 font-medium text-brand-50">Blacklist status</dt>
+                            <dd class="mt-1 text-sm/6 text-brand-200 sm:col-span-2 sm:mt-0">True</dd>
+                            <dt class="text-sm/6 font-medium text-brand-50">Bypass HR Approval</dt>
+                            <dd class="mt-1 text-sm/6 text-brand-200 sm:col-span-2 sm:mt-0">True</dd>
+
+                        </div>
+                        <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+
+
+                        </div>
+                    </dl>
+                </div>
+            </div>
+
 
             <!-- Table Wrapper -->
             <x-table-wrapper>
