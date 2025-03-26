ffe429cfff252d80073aec2891dcae16698be287
mahmud <mahmudmodibbo21@gmail.com>
2025-03-26 22:34:33 +0100

    removed time

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -25,9 +25,7 @@
 
     <!-- Dashboard Cards -->
     <div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto bg-primary">
-        <p class="text-cyan-700">
-            Current time: {{ \Illuminate\Support\Carbon::now()->format('h:i A') }}
-        </p>
+
         <div class="grid sm:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-6">
             <x-dashboard.card
                 title="Total Visitors Today"
