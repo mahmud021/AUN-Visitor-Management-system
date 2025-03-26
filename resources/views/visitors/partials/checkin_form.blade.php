d69016c54dbab2f91657aff42f33d0c1410c84bd
mahmud <mahmudmodibbo21@gmail.com>
2025-03-26 23:21:22 +0100

    pin input regex added

--- resources/views/visitors/partials/checkin_form.blade.php
+++ resources/views/visitors/partials/checkin_form.blade.php
@@ -11,16 +11,33 @@
                 Check In
             </x-primary-button>
         </div>
+
         <div>
             <label for="visitor_code" class="block text-sm font-medium mb-2">
                 Enter Visitor Code
             </label>
+
             <div class="py-2 px-3 bg-brand-900 border border-brand-700 rounded-lg">
-                <div class="flex gap-x-3" data-hs-pin-input>
+                <!--
+                  Note the data-hs-pin-input attribute below includes:
+                  "availableCharsRE": "^[0-9]+$"
+                  so only digits are allowed.
+                -->
+                <div class="flex gap-x-3"
+                     data-hs-pin-input='{
+                       "availableCharsRE": "^[0-9]+$"
+                     }'>
+
                     @for ($i = 1; $i <= 4; $i++)
                         <input
                             id="visitor_code_{{ $i }}"
-                            class="w-8 h-8 text-sm text-center p-0 border-brand-700 rounded-md focus:border-brand-500 focus:ring-brand-500 bg-brand-900 text-brand-200"
+                            class="w-8 h-8 text-sm text-center p-0
+                                   border-brand-700 rounded-md
+                                   focus:border-brand-500 focus:ring-brand-500
+                                   bg-brand-900 text-brand-200
+                                   [appearance:textfield]
+                                   [&::-webkit-outer-spin-button]:appearance-none
+                                   [&::-webkit-inner-spin-button]:appearance-none"
                             type="text"
                             placeholder="○"
                             data-hs-pin-input-item
