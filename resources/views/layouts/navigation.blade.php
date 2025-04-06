cac33f887135781916370c4cacd838efa794fab7
mahmud <mahmudmodibbo21@gmail.com>
2025-04-07 00:46:39 +0100

    side bar icon changed

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,33 +5,28 @@
     <div class="flex items-center py-2">
         <!-- Navigation Toggle -->
         <button type="button"
-                class="size-8 flex justify-center items-center gap-x-2 border border-neutral-700 text-neutral-200 hover:bg-brand-900 focus:outline-none focus:bg-brand-900 disabled:opacity-50 disabled:pointer-events-none"
+                class="size-8 flex justify-center items-center gap-x-2 text-neutral-200 hover:bg-brand-900 focus:outline-none focus:bg-brand-900 disabled:opacity-50 disabled:pointer-events-none"
                 aria-haspopup="dialog" aria-expanded="false" aria-controls="hs-application-sidebar"
                 aria-label="Toggle navigation" data-hs-overlay="#hs-application-sidebar">
             <span class="sr-only">Toggle Navigation</span>
-            <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                 fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                <rect width="18" height="18" x="3" y="3" rx="2"/>
-                <path d="M15 3v18"/>
-                <path d="m8 9 3 3-3 3"/>
-            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-menu-icon lucide-menu"><line x1="4" x2="20" y1="12" y2="12"/><line x1="4" x2="20" y1="6" y2="6"/><line x1="4" x2="20" y1="18" y2="18"/></svg>
         </button>
         <!-- End Navigation Toggle -->
 
         <!-- Breadcrumb -->
-        <ol class="ms-3 flex items-center whitespace-nowrap">
-            <li class="flex items-center text-sm text-neutral-200">
-                Application Layout
-                <svg class="shrink-0 mx-3 overflow-visible size-2.5 text-neutral-500"
-                     width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M5 1L10.6869 7.16086C10.8637 7.35239 10.8637 7.64761 10.6869 7.83914L5 14"
-                          stroke="currentColor" stroke-width="2" stroke-linecap="round"/>
-                </svg>
-            </li>
-            <li class="text-sm font-semibold text-neutral-200 truncate" aria-current="page">
-                Dashboard
-            </li>
-        </ol>
+{{--        <ol class="ms-3 flex items-center whitespace-nowrap">--}}
+{{--            <li class="flex items-center text-sm text-neutral-200">--}}
+{{--                Application Layout--}}
+{{--                <svg class="shrink-0 mx-3 overflow-visible size-2.5 text-neutral-500"--}}
+{{--                     width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">--}}
+{{--                    <path d="M5 1L10.6869 7.16086C10.8637 7.35239 10.8637 7.64761 10.6869 7.83914L5 14"--}}
+{{--                          stroke="currentColor" stroke-width="2" stroke-linecap="round"/>--}}
+{{--                </svg>--}}
+{{--            </li>--}}
+{{--            <li class="text-sm font-semibold text-neutral-200 truncate" aria-current="page">--}}
+{{--                Dashboard--}}
+{{--            </li>--}}
+{{--        </ol>--}}
         <!-- End Breadcrumb -->
     </div>
 </div>
