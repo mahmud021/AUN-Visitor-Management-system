d7e49ef4e2fd3d8393138ce1b00ff03c5059dafe
mahmud <mahmudmodibbo21@gmail.com>
2025-04-07 00:48:06 +0100

    removed log out button in header

--- resources/views/components/dashboard/header.blade.php
+++ resources/views/components/dashboard/header.blade.php
@@ -39,13 +39,13 @@
 
 
         <!-- Logout Form -->
-        <form method="POST" action="{{ route('logout') }}" class="inline">
-            @csrf
-            <button type="submit"
-                    class="inline-flex items-center px-4 py-2 bg-red-600 hover:bg-red-700 border border-transparent rounded-md font-medium text-xs text-white uppercase tracking-widest focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 transition ease-in-out duration-150">
-                {{ __('Log Out') }}
-            </button>
-        </form>
+{{--        <form method="POST" action="{{ route('logout') }}" class="inline">--}}
+{{--            @csrf--}}
+{{--            <button type="submit"--}}
+{{--                    class="inline-flex items-center px-4 py-2 bg-red-600 hover:bg-red-700 border border-transparent rounded-md font-medium text-xs text-white uppercase tracking-widest focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 transition ease-in-out duration-150">--}}
+{{--                {{ __('Log Out') }}--}}
+{{--            </button>--}}
+{{--        </form>--}}
     </div>
 </div>
 
