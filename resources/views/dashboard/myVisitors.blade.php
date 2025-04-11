56ef1ab05400fd56c99a188b27b0ae39c2a44414
mahmud <mahmudmodibbo21@gmail.com>
2025-04-11 19:36:52 +0100

    added can to edit action

--- resources/views/dashboard/myVisitors.blade.php
+++ resources/views/dashboard/myVisitors.blade.php
@@ -106,11 +106,12 @@
                             <span class="block py-2 px-3 text-xs font-medium uppercase text-gray-400 dark:text-neutral-600">
                                 Actions
                             </span>
-
-                                    <a href="{{ route('visitors.edit', $myVisitor->id) }}"
-                                       class="flex items-center gap-x-3 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700 dark:focus:text-neutral-300">
-                                        Edit
-                                    </a>
+                                    @can('update-visitor', $myVisitor)
+                                        <a href="{{ route('visitors.edit', $myVisitor->id) }}"
+                                           class="flex items-center gap-x-3 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700 dark:focus:text-neutral-300">
+                                            Edit
+                                        </a>
+                                    @endcan
 
                                     <a class="flex items-center gap-x-3 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700 dark:focus:text-neutral-300"
                                        href="{{ route('visitors.timeline', $myVisitor->id) }}">
