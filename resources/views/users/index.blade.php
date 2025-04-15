358f265e9eef249dec229dc0a696b472875da258
mahmud <mahmudmodibbo21@gmail.com>
2025-04-15 02:06:52 +0100

    value of search added

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -15,7 +15,7 @@
                             <div class="relative max-w-xs">
                                 <label for="hs-table-search" class="sr-only">Search</label>
                                 <form action="{{ route('user.search') }}" method="GET">
-                                    <input type="text" name="q" class="py-1.5 sm:py-2 px-3 ps-9 block w-full border-gray-200 shadow-2xs rounded-lg sm:text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" placeholder="Search Users">
+                                    <input type="text" name="q" class="py-1.5 sm:py-2 px-3 ps-9 block w-full border-gray-200 shadow-2xs rounded-lg sm:text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" placeholder="Search Users" value="{{ request()->input('q') }}">
                                 </form>
                                     <div class="absolute inset-y-0 start-0 flex items-center pointer-events-none ps-3">
                                     <svg class="size-4 text-gray-400 dark:text-neutral-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
