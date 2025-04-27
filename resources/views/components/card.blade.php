66c2923197a58e312162fea1b26e4a5eea68b7c6
mahmud <mahmudmodibbo21@gmail.com>
2025-04-27 19:04:04 +0100

    My visitors fixed

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,3 +1,14 @@
-<div {{ $attributes->merge(['class' => 'p-4 md:p-5 group flex flex-col dark:bg-brand-900 border border-neutral-800 shadow-sm rounded-xl hover:shadow-md focus:outline-none focus:shadow-md transition hover:border-1 hover:border-white']) }}>
+{{-- Generic dark-mode card wrapper --}}
+<div
+    {{ $attributes->merge([
+        'class' => '
+            relative flex flex-col gap-y-3
+            p-5 rounded-xl
+            bg-neutral-900/60 text-gray-100
+            ring-1 ring-neutral-800 shadow-sm
+            transition hover:ring-blue-600 focus-within:ring-blue-600
+        ',
+    ]) }}
+>
     {{ $slot }}
 </div>
